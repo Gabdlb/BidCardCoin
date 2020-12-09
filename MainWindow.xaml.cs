1716ca2e904f2c2d29db3e74dd9511a72b4244b7
Gabrieldlb <gabriel.delabiche@epsi.fr>
2020-12-09 13:05:39 +0100

    navbar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BidCardCoin
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
