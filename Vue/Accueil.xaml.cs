383a6791b83e941b6c96506a2fca073824af3ab5
Gabrieldlb <gabriel.delabiche@epsi.fr>
2020-12-14 17:07:11 +0100

    Accueil + vues

---
+++ Vue/Accueil.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BidCardCoin.Vue
+{
+    /// <summary>
+    /// Logique d'interaction pour Accueil.xaml
+    /// </summary>
+    public partial class Accueil : Window
+    {
+        public Accueil()
+        {
+            InitializeComponent();
+
+
+        }
+
+       
+
+        private void Button_Article(object sender, RoutedEventArgs e)
+        {
+            accueil.Content = new Article();
+        }
+
+        private void Button_Vente(object sender, RoutedEventArgs e)
+        {
+            accueil.Content = new Vente();
+        }
+
+        private void Button_Lieu(object sender, RoutedEventArgs e)
+        {
+            accueil.Content = new Lieu();
+        }
+
+        private void Button_Comm(object sender, RoutedEventArgs e)
+        {
+            accueil.Content = new Commissaire();
+        }
+
+        private void Button_Utilisateur(object sender, RoutedEventArgs e)
+        {
+            accueil.Content = new Utilisateur();
+        }
+    }
+}
